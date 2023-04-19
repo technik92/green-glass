6d9a763bd94ea29a5dc67b2c0dc90866b083ffaf
Kamil Sobik <kamil.sobik@gmail.com>
2023-04-19 10:56:33 +0200

    add package splide and config

--- src/partials/Carousel.jsx
+++ src/partials/Carousel.jsx
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Image from "next/image";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 import image1 from "../images/carousel/001.jpg";
 import image2 from "../images/carousel/002.jpg";
@@ -8,7 +10,6 @@ import image4 from "../images/carousel/004.jpg";
 import image5 from "../images/carousel/005.jpeg";
 import image6 from "../images/carousel/006.jpeg";
 import image7 from "../images/carousel/007.jpg";
-import image8 from "../images/carousel/008.jpeg";
 import image9 from "../images/carousel/009.jpeg";
 import image10 from "../images/carousel/010.jpg";
 import image11 from "../images/carousel/011.jpg";
@@ -17,86 +18,20 @@ import image13 from "../images/carousel/013.jpg";
 import image14 from "../images/carousel/014.jpg";
 import image15 from "../images/carousel/015.jpg";
 import image16 from "../images/carousel/016.jpg";
-import image17 from "../images/carousel/017.jpeg";
-import image18 from "../images/carousel/018.jpeg";
 import image19 from "../images/carousel/019.jpg";
 import image20 from "../images/carousel/020.jpeg";
 import image21 from "../images/carousel/021.jpg";
 import image22 from "../images/carousel/022.jpg";
 import image23 from "../images/carousel/023.jpg";
-import image24 from "../images/carousel/024.jpg";
 import image25 from "../images/carousel/025.jpeg";
 import image26 from "../images/carousel/026.jpeg";
 
-import { ArrowRightIcon, ArrowLeftIcon } from "@/icons";
-
-const Carousel2 = ({ header, description }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const slides = [
-    image1,
-    image2,
-    image3,
-    image4,
-    image5,
-    image6,
-    image7,
-    image8,
-    image9,
-    image10,
-    image11,
-    image12,
-    image13,
-    image14,
-    image15,
-    image16,
-    image17,
-    image18,
-    image19,
-    image20,
-    image21,
-    image22,
-    image23,
-    image24,
-    image25,
-    image26,
-  ];
-
-  const infiniteScroll = () => {
-    if (currentIndex === slides.length - 2) {
-      return setCurrentIndex(0);
-    }
-    return setCurrentIndex((prev) => prev + 1);
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => infiniteScroll(), 3000);
-
-    return () => clearInterval(interval);
-  });
-
-  const handleNextSlide = () => {
-    if (currentIndex === slides.length - 2) {
-      setCurrentIndex(0);
-    } else {
-      setCurrentIndex((prev) => prev + 1);
-    }
-  };
-
-  const handlePrevSlide = () => {
-    if (currentIndex === 0) {
-      setCurrentIndex(slides.length - 2);
-    } else {
-      setCurrentIndex((prev) => prev - 1);
-    }
-  };
-
+const Carousel = ({ header, description }) => {
   return (
     <>
       <section>
         <div id="galeria" className="max-w-6xl mx-auto px-4 sm:px-6">
           <div className="py-12 md:py-20 border-t border-zinc-700">
-            {/* Section header */}
             <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
               <h2 className="h2" data-aos="fade-up">
                 {header}
@@ -105,8 +40,6 @@ const Carousel2 = ({ header, description }) => {
                 {description}
               </p>
             </div>
-
-            {/* Carousel */}
             <div
               className="max-w-sm mx-auto md:max-w-none"
               data-aos="fade-left"
@@ -119,41 +52,74 @@ const Carousel2 = ({ header, description }) => {
                     data-aos="fade-up"
                     data-aos-anchor="[data-aos-id-carousel]"
                   >
-                    {slides.map((slide, index) => (
-                      <Image
-                        key={index}
-                        src={slide}
-                        className="carousel__item"
-                        alt="slide"
-                        style={{
-                          transform: `translate(-${currentIndex * 100}%)`,
-                          minWidth: "50%",
-                          width: "50%",
-                        }}
-                      />
-                    ))}
-
-                    <div className="carousel__controls">
-                      <button type="button" onClick={handlePrevSlide}>
-                        <Image
-                          alt="left arrow"
-                          src={ArrowLeftIcon}
-                          height="auto"
-                          width="auto"
-                          className="controls__arrow"
-                        />
-                      </button>
-
-                      <button type="button" onClick={handleNextSlide}>
-                        <Image
-                          alt="left arrow"
-                          src={ArrowRightIcon}
-                          height="auto"
-                          width="auto"
-                          className="controls__arrow"
-                        />
-                      </button>
-                    </div>
+                    <Splide aria-labelledby="GreenGlassImages">
+                      <SplideSlide>
+                        <Image src={image1} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image2} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image3} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image4} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image5} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image6} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image7} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image9} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image10} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image11} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image12} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image13} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image14} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image15} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image16} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image19} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image20} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image21} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image22} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image23} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image25} alt="Repair car window" />
+                      </SplideSlide>
+                      <SplideSlide>
+                        <Image src={image26} alt="Repair car window" />
+                      </SplideSlide>
+                    </Splide>
                   </div>
                 </div>
               </div>
@@ -165,4 +131,4 @@ const Carousel2 = ({ header, description }) => {
   );
 };
 
-export default Carousel2;
+export default Carousel;
