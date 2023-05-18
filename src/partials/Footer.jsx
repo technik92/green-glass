f8053c84210d7847509c34ce6aea43a1b1462b54
Kamil Sobik <kamil.sobik@gmail.com>
2023-05-18 14:44:47 +0200

    changing footer

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="max-w-3xl mx-auto text-center  py-12 md:py-16">
           <div className="text-gray-400 text-sm mr-4">
-            &copy; DevPro. All rights reserved.
+            &copy; Green Glass. All rights reserved. Created by DevPro.
           </div>
         </div>
       </div>
