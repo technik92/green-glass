f26d9f43ddaedd1845bf67f6048c5720dc57ba56
Kamil Sobik <kamil.sobik@gmail.com>
2023-05-19 13:25:45 +0200

    changing testimonials components - delete avatars

--- src/partials/Testimonials.jsx
+++ src/partials/Testimonials.jsx
@@ -1,9 +1,4 @@
-import Image from "next/image";
 import React from "react";
-
-import TestimonialImage01 from "../images/testimonial-01.jpg";
-import TestimonialImage02 from "../images/testimonial-02.jpg";
-import TestimonialImage03 from "../images/testimonial-03.jpg";
 
 function Testimonials({
   header,
@@ -28,24 +23,6 @@ function Testimonials({
               className="flex flex-col h-full p-6 bg-gray-800"
               data-aos="fade-up"
             >
-              <div>
-                <div className="relative inline-flex flex-col mb-4">
-                  <Image
-                    className="rounded-full"
-                    src={TestimonialImage01}
-                    width="48"
-                    height="48"
-                    alt="Testimonial 01"
-                  />
-                  <svg
-                    className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-green-600"
-                    viewBox="0 0 24 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
-                  </svg>
-                </div>
-              </div>
               <blockquote className="text-lg text-gray-400 grow">
                 {firstClientOpinion}
               </blockquote>
@@ -53,7 +30,6 @@ function Testimonials({
                 <cite className="text-gray-200 not-italic">
                   {firstClientNames}
                 </cite>
-                -
               </div>
             </div>
             <div
@@ -61,24 +37,6 @@ function Testimonials({
               data-aos="fade-up"
               data-aos-delay="200"
             >
-              <div>
-                <div className="relative inline-flex flex-col mb-4">
-                  <Image
-                    className="rounded-full"
-                    src={TestimonialImage02}
-                    width="48"
-                    height="48"
-                    alt="Testimonial 02"
-                  />
-                  <svg
-                    className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-green-600"
-                    viewBox="0 0 24 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
-                  </svg>
-                </div>
-              </div>
               <blockquote className="text-lg text-gray-400 grow">
                 {secondClientOpinion}
               </blockquote>
@@ -93,24 +51,6 @@ function Testimonials({
               data-aos="fade-up"
               data-aos-delay="400"
             >
-              <div>
-                <div className="relative inline-flex flex-col mb-4">
-                  <Image
-                    className="rounded-full"
-                    src={TestimonialImage03}
-                    width="48"
-                    height="48"
-                    alt="Testimonial 03"
-                  />
-                  <svg
-                    className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-green-600"
-                    viewBox="0 0 24 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
-                  </svg>
-                </div>
-              </div>
               <blockquote className="text-lg text-gray-400 grow">
                 {thirdClientOpinion}
               </blockquote>
@@ -126,4 +66,5 @@ function Testimonials({
     </section>
   );
 }
+
 export default Testimonials;
